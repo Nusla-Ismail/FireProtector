5baafa231d0ef52c659d51c295512d7dfd3f8875
sanjuwa <sanjulahasaranga0@gmail.com>
2023-02-28 12:02:41 +0530

    Create Tile Component

--- frontend/lib/views/previous_fires.dart
+++ frontend/lib/views/previous_fires.dart
@@ -1,4 +1,5 @@
 import 'package:fireprotector/constants.dart';
+import 'package:fireprotector/widgets/tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -38,50 +39,7 @@ class PreviousFires extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.horizontal(right: Radius.circular(10.r)),
-                ),
-                elevation: 3,
-                margin: EdgeInsets.only(bottom: 16.h),
-                child: IntrinsicHeight(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 15.w),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "2020-02-10",
-                              style: Theme.of(context).textTheme.headline3,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(5.w),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: kBtnAsh,
-                            borderRadius: BorderRadius.horizontal(right: Radius.circular(10.r)),
-                          ),
-                          child: SizedBox(
-                            width: 40.w,
-                            child: Icon(
-                              Icons.arrow_forward_ios,
-                              color: Colors.white,
-                              size: 30.w,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              Tile(),
             ],
           ),
         ),
