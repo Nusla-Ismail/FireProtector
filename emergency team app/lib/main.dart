78bb5e1c8a7ce6517c1ee4a02b2db08277fca034
ASUS <tharusha.20210632@iit.ac.lk>
2023-03-25 22:56:47 +0530

    add

--- emergency team app/lib/main.dart
+++ emergency team app/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
-
 import 'constants.dart';
 import 'home.dart';
 
