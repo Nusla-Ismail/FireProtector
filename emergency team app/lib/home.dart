a4fa3777c15428be7bb3dd349144f73a2a0d6001
ASUS <tharusha.20210632@iit.ac.lk>
2023-03-30 22:56:59 +0530

    change

--- emergency team app/lib/home.dart
+++ emergency team app/lib/home.dart
@@ -4,13 +4,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'contact.dart';
 
 class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
