0fa8cb585bcf7e2560cbc6fba4ce0217b92394b0
Nusla Ismail <124285876+Nusla-Ismail@users.noreply.github.com>
2023-04-05 13:09:38 +0530

    added the fire cases

--- emergency team app/lib/confirmation.dart
+++ emergency team app/lib/confirmation.dart
@@ -4,7 +4,6 @@ import 'package:fireprotector_emergency_team/widgets/medium_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'constants.dart';
 
 class Confirmation extends StatelessWidget {
